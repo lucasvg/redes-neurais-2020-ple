de64d79ebd72131e0867374eaa9ba3e2d7c93340
Lucas Vieira <lucasvg@poli.ufrj.br>
2020-10-24 23:44:23 -0300

    Adds biases

--- exercicio7/NN.py
+++ exercicio7/NN.py
@@ -1,32 +1,43 @@
 class NN:
     """
     weightsMatrix input example: [[[0.1, 0.2, 0.2], [0.1, 0.1, 0.1]]]
-    This represents network with 3 inputs, and one layer with 2 neurons and two outputs
+    This represents network of:
+        2 inputs
+        1 layer of 2 neurons
+        2 outputs
 
     Default activation functions is ReLU. Any neuron's activaction function can be overriden using the method setActivationFunction
     """
-
-    customActivationFunctions = [[]]
+    DEBUG = False
+    customActivationFunctions = [[]],
 
     def __init__(self, weightsMatrix):
         self.weightsMatrix = weightsMatrix
+        self.biases = [1 for i in range(len(weightsMatrix))]
 
     def setActivationFunction(self):
         print("setActivationFunction")
 
     def predict(self, input):
-        for layer in self.weightsMatrix:
+        for idxLayer in range(len(self.weightsMatrix)):
+            layer = self.weightsMatrix[idxLayer]
             nextInput = []
             for idxNeuron in range(len(layer)):
-                integration = 0
                 neuronEdges = layer[idxNeuron]
-                if(len(input)!=len(neuronEdges)):
-                    raise ArithmeticError("Something went wront! (Input amount "+str(input)+" doesnt match neuron's connections amount "+str(neuronEdges))
-                for idxEdge in range(len(neuronEdges)):
-                    integration += neuronEdges[idxEdge] * input[idxEdge]
+                
+                if(len(input)+1!=len(neuronEdges)):
+                    raise ArithmeticError("Something went wront! (Input amount "+str(input)+" + 1 bias doesnt match neuron's connections amount "+str(neuronEdges))
+                
+                integration = neuronEdges[0]*self.biases[idxLayer]
+                for idxEdge in range(len(input)):
+                    if(self.DEBUG):
+                        print(neuronEdges[idxEdge+1], input[idxEdge], neuronEdges[idxEdge+1] * input[idxEdge])
+                    integration += neuronEdges[idxEdge+1] * input[idxEdge]
                 activationFunction = self.ReLU
                 nextInput.append(activationFunction(integration))
             input = nextInput
+            if(self.DEBUG):
+                print(str(idxLayer) + " layer: "+ str(input))
         return input
             
     def ReLU(self, value):
--- exercicio7/program2.py
+++ exercicio7/program2.py
@@ -7,7 +7,15 @@ DEBUG = True
 
 layersWeights = [
     [[0.1, 0.2, 0.2], [0.1, 0.1, 0.1]],
-    [[.4, .1]]
+    [[.4, .1, .2]]
 ]
 myNN = NN(layersWeights)
-print(myNN.predict([0.1, 0.1, 0.1]))
+print(myNN.predict([0.1, 0.1]))
+
+layersWeights = [
+    [[-.3, .1, .0], [0.2, 0.1, 0.4]],
+    [[.3, .0, -.1], [0.4, 0, .4], [0.1, 0.3, 0.0]],
+    [[0.3, -.1, 0.5, -.1], [0.4, 0.3, -.2, .4]]
+]
+myNN = NN(layersWeights)
+print(myNN.predict([-0.3, 0.5]))
